ab216400fd0dc2547cbb0d49c9058210037ec23d
rsvzuiun <87635034+rsvzuiun@users.noreply.github.com>
2023-09-05 00:39:58 +0900

    装備要求レベル計算機

---
+++ content/posts/equip-lv/index.js
@@ -0,0 +1,62 @@
+let storage = undefined;
+
+const clip = (x, l, u) => {
+  if (x < l) {
+    return l;
+  } else if (u < x) {
+    return u;
+  } else {
+    return x;
+  }
+};
+
+const doit = () => {
+  const base = parseInt(document.getElementById("base").value) || 0;
+  const nx_minus = parseInt(document.getElementById("nx_minus").value) || 0;
+  const badge_minus =
+    parseInt(document.getElementById("badge_minus").value) || 0;
+  const op1 = parseInt(document.getElementById("op1").value);
+  const op2 = parseInt(document.getElementById("op2").value);
+  const op3 = parseInt(document.getElementById("op3").value);
+  const method = document.getElementById("method").value;
+
+  const ops = [op1, op2, op3].sort((a, b) => {
+    if (isNaN(a)) return 1;
+    else if (isNaN(b)) return -1;
+    else return b - a;
+  });
+  let item = clip(base - nx_minus - badge_minus, 0, Infinity);
+  console.log(ops);
+  if (method == "UM") {
+    if (!isNaN(ops[2])) {
+      item +=
+        Math.trunc((ops[0] * 3) / 4) +
+        Math.trunc(ops[1] / 4) +
+        Math.trunc(ops[2] / 20);
+    } else if (!isNaN(ops[1])) {
+      item += Math.trunc((ops[0] * 3) / 4) + Math.trunc(ops[1] / 4);
+    } else if (!isNaN(ops[0])) {
+      item += ops[0];
+    }
+  } else {
+    if (!isNaN(ops[0])) item += ops[0];
+    if (!isNaN(ops[1])) item += Math.trunc((ops[1] * 2) / 3);
+    if (!isNaN(ops[2])) item += Math.trunc((ops[2] * 1) / 3);
+  }
+  document.getElementById("out").innerText = item;
+
+  storage.save();
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const elms_in = document.querySelectorAll(".in");
+  elms_in.forEach((e, k, p) => {
+    e.addEventListener("input", doit);
+  });
+  storage = new FormStorage("form", {
+    name: "rsvzui-equip-lv",
+    includes: ['[class="in"]'],
+  });
+  storage.apply();
+  doit();
+});
---
+++ content/posts/equip-lv/index.md
@@ -0,0 +1,54 @@
+---
+date: 2023-09-04T23:45:49+09:00
+title: "装備要求レベル計算機"
+description: ""
+tags: ["計算機"]
+math: true
+---
+
+<script defer src="/js/form-storage.js"></script>
+<script defer src="index.js"></script>
+
+<form action="javascript:void(0);">
+  <table>
+    <tr>
+      <td>ベース要求Lv</td>
+      <td><input type="number" name="base" id="base" class="in" value="1100"></td>
+    </tr>
+    <tr>
+      <td>Nx1OP目</td>
+      <td>- <input type="number" name="nx_minus" id="nx_minus" class="in" value="180"></td>
+    </tr>
+    <tr>
+      <td>バッチなど</td>
+      <td>- <input type="number" name="badge_minus" id="badge_minus" class="in" value="0"></td>
+    </tr>
+    <tr>
+      <td>1OP *</td>
+      <td>+ <input name="op1" id="op1" class="in" value="-"></td>
+    </tr>
+    <tr>
+      <td>2OP *</td>
+      <td>+ <input name="op2" id="op2" class="in" value="-"></td>
+    </tr>
+    <tr>
+      <td>3OP *</td>
+      <td>+ <input name="op3" id="op3" class="in" value="-"></td>
+    </tr>
+    <tr>
+      <td>N / UM</td>
+      <td>
+      <select name="method" id="method" class="in">
+      <option value="N">N</option>
+      <option value="UM">UM</option>
+      </select>
+      </td>
+    </tr>
+    <tr>
+      <td>要求レベル</td>
+      <td><span id="out"></span></td>
+    </tr>
+  </table>
+  <p>* 空オプションは"-"など数字ではないものを入力して、NPCオプションなど要求レベル0のオプションは0を入力してください。</p>
+</form>
+
